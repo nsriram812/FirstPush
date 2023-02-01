af237cab156227d2e681f7eb75e804d0900fb0f6
nsriram812 <nsriram0@frostburg.edu>
2023-02-01 11:10:00 -0500

    First commit

---
+++ Palindrome.java
@@ -0,0 +1,21 @@
+import java.util.*
+
+Class Palindrome{
+ Pulic Static Void main(String args[])
+{
+	int sum=0,temp,var;
+	int number=818;
+	temp=number;
+	while(n>0)
+	{
+		var=number%10;
+		sum=(sum*10)+var;
+		mumber=number/10;
+	}
+	if(temp==sum)
+		System.out.println("The number is a palindrome");
+	else
+		System.out.println("The number is not a palindrome);
+}
+}
+	
